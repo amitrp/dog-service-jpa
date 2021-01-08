a5368696532482a3c18c1e49ce1c70f678158d8b
Amit Phaltankar <amitphaltankar@amitsofficemac.modem>
2021-01-08 12:16:14 +1100

    Wildcard_examples

--- src/main/java/com/amitph/spring/dogs/repo/Dog.java
+++ src/main/java/com/amitph/spring/dogs/repo/Dog.java
@@ -1,16 +1,25 @@
 package com.amitph.spring.dogs.repo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Dog {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private int age;
+
+    public Dog(){}
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public long getId() {
         return id;
@@ -34,5 +43,10 @@ public class Dog {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "id: " + this.id + ", name: " + this.name + ", age: "+this.age;
     }
 }
